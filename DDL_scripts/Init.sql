dd058223d05128c566b08cdb432d40c8cb2220c2
Ksenya Kurinova <68475716+ksinus@users.noreply.github.com>
2022-04-12 20:16:01 +0300

    Update Init.sql

--- DDL_scripts/Init.sql
+++ DDL_scripts/Init.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS Users
     user_nm         VARCHAR(20) NOT NULL,
     user_last_n     VARCHAR(20) NOT NULL,
     user_birth_dt   DATE,
-    mobile_phone_no INTEGER UNIQUE,
+    mobile_phone_no VARCHAR(20) UNIQUE,
     email_no        VARCHAR(20) UNIQUE,
-    card_num        VARCHAR(40),
+    card_no         VARCHAR(40) DEFAULT NULL,
     register_dt     DATE DEFAULT now()::date
 );
 
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS Categories
 
 CREATE TABLE IF NOT EXISTS Advert_price_history
 (
-    advert_id   INTEGER,
-    price_amt   INTEGER,
+    advert_id INTEGER,
+    price_amt INTEGER,
     advert_dt DATE DEFAULT now()::date
 );
 
@@ -39,4 +39,3 @@ CREATE TABLE IF NOT EXISTS Favorites
     advert_id INTEGER NOT NULL,
     user_id   INTEGER NOT NULL
 );
-
