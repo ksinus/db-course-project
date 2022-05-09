4f77ac15d9d7c4f9a64b84dbc6cf01a8e8c736f4
Ksenya Kurinova <68475716+ksinus@users.noreply.github.com>
2022-05-09 15:16:44 +0300

    Add files via upload

--- DDL_scripts/MakeIndex.sql
+++ DDL_scripts/MakeIndex.sql
@@ -2,14 +2,14 @@
 Создадим индекс по выражению lower(title),
 которое позволяет эффективно выполнять регистронезависимый поиск
 */
-CREATE INDEX ON adverts ((lower(advert_title)));
+CREATE INDEX IF NOT EXISTS searchByTitle ON adverts ((lower(advert_title))) ;
 /*
 Создадим индекс по айди пользователя, чтобы быстрее получать
 группу избранных для выдачи
 */
-CREATE INDEX favoritesByUser ON favorites (user_id);
+CREATE INDEX IF NOT EXISTS favoritesByUser ON favorites (user_id);
 /*
 Создадим индекс, поддерживающий сортировку имён пользователей в алфавитном
 порядке, что позволит быстрее искать человека по ФИО
 */
-CREATE INDEX ON users (user_nm, user_last_n DESC);
+CREATE INDEX IF NOT EXISTS sortByUsers ON users (user_nm, user_last_n DESC);
